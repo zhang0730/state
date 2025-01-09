cca6d1d2f2bc483e0feb17e6ba009fcaeb29cced
Rajesh Ilango <rajesh.ilango@arcinstitute.org>
2025-01-09 12:11:53 -0800

    Change the encoder to become a generator function

--- vci/inference.py
+++ vci/inference.py
@@ -1,124 +1,76 @@
 import os
-import sys
-import h5py
-import hydra
 import logging
-import numpy as np
-
-from omegaconf import DictConfig
-
 import torch
+
+from omegaconf import OmegaConf
 from torch import nn
 from torch.utils.data import DataLoader
-from torch.nn import BCEWithLogitsLoss
 
-sys.path.append('.')
-from vci.data import H5adDatasetSentences, VCIDatasetSentenceCollator
 from vci.model import LitUCEModel
 from vci.train.trainer import get_ESM2_embeddings
+from vci.data import H5adDatasetSentences, VCIDatasetSentenceCollator
 
 
 log = logging.getLogger(__name__)
 
 
-checkpoint = '/scratch/ctc/vci/checkpoint/pre_1/exp_pre_1_layers_4_dmodel_512_samples_2048_max_lr_0.0004_op_dim_256-epoch=1-step=91280.ckpt'
-emb_file = '/large_storage/goodarzilab/userspace/mohsen/VCI/ZeroShotCellType_Replogle_jurkat_edist_train/predictions/scvi_pred_map_random.h5ad'
+class Inference():
 
-def update_data(embeddings, losses, f):
-    embeddings = np.vstack(embeddings)
-    losses = np.vstack(losses)
-    log.info(f'Loss {losses.mean()}')
-    f['embedding'].resize((f['embedding'].shape[0] + embeddings.shape[0]), axis=0)
-    f['embedding'][-embeddings.shape[0]:] = embeddings
+    def __init__(self, cfg):
+        self._vci_conf = cfg
+        self.model = None
+        self.collator = None
 
-    f['loss'].resize((f['loss'].shape[0] + losses.shape[0]), axis=0)
-    f['loss'][-losses.shape[0]:] = losses
+    def load_model(self, checkpoint):
+        # Load and initialize model for eval
 
+        self.model = LitUCEModel.load_from_checkpoint(checkpoint)
+        all_pe = get_ESM2_embeddings(self._vci_conf)
+        all_pe.requires_grad = False
+        self.model.pe_embedding = nn.Embedding.from_pretrained(all_pe)
+        self.model.pe_embedding.to(self.model.device)
+        self.model.binary_decoder.requires_grad = False
+        self.model.eval()
 
-@hydra.main(config_path="../conf", config_name="defaults")
-def main(cfg: DictConfig):
-    log.info(f'Embeddings will be saved at {emb_file}')
-    log.info(f'Loading checkpoint from {checkpoint}')
-    model = LitUCEModel.load_from_checkpoint(checkpoint)
-    all_pe = get_ESM2_embeddings(cfg)
-    all_pe.requires_grad= False
-    model.pe_embedding = nn.Embedding.from_pretrained(all_pe)
-    model.pe_embedding.to(model.device)
-    model.binary_decoder.requires_grad= False
-    model.eval()
+    def create_dataloader(self,
+                          datasets,
+                          shape_dict,
+                          batch_size=32,
+                          workers=1,
+                          data_dir=None):
+        if data_dir:
+            self._vci_conf.dataset.data_dir = data_dir
 
-    # Setup Data
-    dataset = H5adDatasetSentences(cfg)
-    multi_dataset_sentence_collator = VCIDatasetSentenceCollator(cfg)
+        dataset = H5adDatasetSentences(self._vci_conf,
+                                       datasets=datasets,
+                                       shape_dict=shape_dict)
+        sentence_collator = VCIDatasetSentenceCollator(self._vci_conf)
+        dataloader = DataLoader(dataset,
+                                batch_size=batch_size,
+                                shuffle=False,
+                                collate_fn=sentence_collator,
+                                num_workers=workers,
+                                persistent_workers=True)
+        return dataloader
 
-    # Make the dataloader outside of the
-    dataloader = DataLoader(dataset,
-                            batch_size=1,
-                            shuffle=False,
-                            collate_fn=multi_dataset_sentence_collator,
-                            num_workers=1,
-                            persistent_workers=True)
+    def encode(self, dataloader):
+        with torch.no_grad():
+            for i, batch in enumerate(dataloader):
+                torch.cuda.empty_cache()
+                batch_sentences = batch[0].to(self.model.device)
+                mask = batch[1].to(self.model.device)
 
-    embeddings = []
-    losses = []
-    f = None
-    with torch.no_grad():
-        i = 1
-        criterion = BCEWithLogitsLoss()
-        for i, batch in enumerate(dataloader):
-            torch.cuda.empty_cache()
+                batch_sentences = self.model.pe_embedding(batch_sentences.long())
+                batch_sentences = nn.functional.normalize(batch_sentences, dim=2)
+                gene_output, embedding = self.model(batch_sentences, mask=mask)
+                embeddings = embedding.detach().cpu().numpy()
 
-            batch_sentences = batch[0].to(model.device)
-            mask = batch[1].to(model.device)
-            X = batch[2].to(model.device)
-            Y = batch[3].to(model.device).squeeze()
-            # dataset_nums = batch[5].to(model.device)
+                yield embeddings
 
-            batch_sentences = model.pe_embedding(
-                batch_sentences.long())
-            batch_sentences = nn.functional.normalize(batch_sentences, dim=2)
-            gene_output, embedding = model(batch_sentences, mask=mask)
-
-            embeddings.append(embedding.detach().cpu().numpy())
-
-            # dataset_num_emb = model.dataset_num_embedding(dataset_nums)
-            X = model.pe_embedding(X.long())
-            X = model.gene_embedding_layer(X)
-
-            embedding = embedding.unsqueeze(1).repeat(1, X.shape[1], 1)
-            # dataset_num_emb = dataset_num_emb.unsqueeze(1).repeat(1, X.shape[1], 1)
-
-            combine = torch.cat((X, embedding), dim=2)
-            decs = model.binary_decoder(combine).squeeze()
-
-            loss = criterion(input=decs, target=Y)
-            losses.append(loss.detach().cpu().numpy())
-            i = i + 1
-
-            if i % 10 == 0:
-                if not os.path.exists(emb_file):
-                    embeddings = np.vstack(embeddings)
-                    losses = np.vstack(losses)
-                    with h5py.File(emb_file, "a") as h5f:
-                        h5f.create_dataset('embedding',
-                                        chunks=True,
-                                        data=embeddings,
-                                        maxshape=(None, 256))
-                        h5f.create_dataset('loss',
-                                        chunks=True,
-                                        data=losses,
-                                        maxshape=(None, 1))
-                else:
-                    with h5py.File(emb_file, "a") as h5f:
-                        update_data(embeddings, losses, h5f)
-
-                embeddings = []
-                losses = []
-
-        # if embeddings and losses:
-        #     with h5py.File(emb_file, "a") as h5f:
-        #         update_data(embeddings, losses, h5f)
-
-
-if __name__ == "__main__":
-    main()
+    def decode(self, adata_path: str, emb_key: str):
+        # X = self.model.pe_embedding(X.long())
+        # X = self.model.gene_embedding_layer(X)
+        # embedding = embedding.unsqueeze(1).repeat(1, X.shape[1], 1)
+        # combine = torch.cat((X, embedding), dim=2)
+        # decs = self.model.binary_decoder(combine).squeeze()
+        pass
