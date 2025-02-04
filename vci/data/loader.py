Fix for passing count for loss computation

vci/data/loader.py
@@ -180,16 +180,18 @@ class VCIDatasetSentenceCollator(object):
 
         dataset_nums = torch.zeros(batch_size)
 
-        # return self.sample_cell_sentences_batched(batch)
-
+        largest_cnt = max([x[0].shape[1] for x in batch])
+        batch_weights = torch.zeros((batch_size, largest_cnt))
         i = 0
         max_len = 0
 
         for counts, idx, dataset, dataset_num in batch:
-            (bs, msk, xx, yy) = self.sample_cell_sentences(counts, dataset)
+            (bs, msk, xx, yy, batch_weight) = self.sample_cell_sentences(counts, dataset)
 
             yy = yy.squeeze()
             batch_sentences[i, :] = bs
+            batch_weight = batch_weight.squeeze()
+            batch_weights[i, :len(batch_weight)] = batch_weight
 
             max_len = max(max_len, self.cfg.dataset.pad_length)
             mask[i, :] = msk
@@ -205,19 +207,20 @@ class VCIDatasetSentenceCollator(object):
             mask[:, :max_len],
             Xs,
             Ys,
-            idxs
+            idxs,
+            batch_weights
         )
 
-    def sample_cell_sentences_batched(self, batch):
-        cnts = []
-        for counts, idx, dataset, dataset_num in batch:
-            cnts.append(counts)
+    # def sample_cell_sentences_batched(self, batch):
+    #     cnts = []
+    #     for counts, idx, dataset, dataset_num in batch:
+    #         cnts.append(counts)
 
-        batch_weights = torch.log1p(counts)
-        batch_weights = batch_weights / torch.sum(batch_weights)
+    #     batch_weights = torch.log1p(counts)
+    #     batch_weights = batch_weights / torch.sum(batch_weights)
 
-        cell_sentences_pe, mask, cell_outputs_X_pe, cell_outputs_Y = None
-        return (cell_sentences_pe, mask, cell_outputs_X_pe, cell_outputs_Y)
+    #     cell_sentences_pe, mask, cell_outputs_X_pe, cell_outputs_Y = None
+    #     return (cell_sentences_pe, mask, cell_outputs_X_pe, cell_outputs_Y)
 
 
     def softmax(self, x):
@@ -225,6 +228,8 @@ class VCIDatasetSentenceCollator(object):
         return e_x / e_x.sum()
 
     def sample_cell_sentences(self, counts, dataset):
+        if torch.isnan(counts).any():
+            log.error(f"NaN values in counts for dataset {dataset}")
         batch_weights = torch.log1p(counts)
         batch_weights = (batch_weights / torch.sum(batch_weights))
 
@@ -261,14 +266,13 @@ class VCIDatasetSentenceCollator(object):
 
             i = 1  # continue on to the rest of the sequence with left bracket being assumed.\
             ordered_choice_idx[i:(self.cfg.dataset.pad_length)] = dataset_idxs[choice_idx]
-            i = i + self.cfg.dataset.pad_length - 1
+            i = i + len(choice_idx) - 1
 
             remainder_len = ( self.cfg.dataset.pad_length - i)
-
             cell_mask = torch.concat((torch.zeros(i, dtype=bool),
                                       torch.ones(remainder_len, dtype=bool)))
-
             mask[c, :] = cell_mask
+            # mask[c, mask_weights] = 1
 
             ordered_choice_idx[i:] =  self.cfg.dataset.pad_token_idx  # mask
 
@@ -297,4 +301,4 @@ class VCIDatasetSentenceCollator(object):
         cell_outputs_X_pe = dataset_idxs[
             cell_outputs_X.long()]  # .unsqueeze(2) # all_pe[dataset_idxs[cell_outputs_X.long()], :]
 
-        return cell_sentences_pe, mask, cell_outputs_X_pe, cell_outputs_Y
+        return cell_sentences_pe, mask, cell_outputs_X_pe, cell_outputs_Y, batch_weights

vci/nn/loss.py
@@ -64,13 +64,22 @@ class WassersteinLoss(nn.Module):
 
 
 class KLDivergenceLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, epsilon=1e-10):
         super().__init__()
+        self.epsilon = epsilon
 
     def forward(self, input, target):
-        # Convert logits to probabilities
+        input = torch.nan_to_num(input, nan=0.0)
+        target = torch.nan_to_num(target, nan=0.0)
+
+        max_len = max(input.size(1), target.size(1))
+        if input.size(1) < max_len:
+            input = F.pad(input, (0, max_len - input.size(1)), 'constant', 0)
+        if target.size(1) < max_len:
+            target = F.pad(target, (0, max_len - target.size(1)), 'constant', 0)
+
         p = F.softmax(input, dim=-1)
         q = F.softmax(target, dim=-1)
 
-        # return F.kl_div(q.log(), p, reduction='batchmean')
+        # return F.kl_div(q, p, reduction='batchmean')
         return torch.sum(p * torch.log(p / q))

vci/nn/model.py
@@ -26,13 +26,13 @@ from vci.utils import compute_gene_overlap_cross_pert
 from .loss import WassersteinLoss, KLDivergenceLoss
 
 
-def full_block(in_features, out_features, p_drop=0.1):
-    return nn.Sequential(
-        nn.Linear(in_features, out_features, bias=True),
-        nn.LayerNorm(out_features),
-        nn.GELU(),
-        nn.Dropout(p=p_drop),
-    )
+# def full_block(in_features, out_features, p_drop=0.1):
+#     return nn.Sequential(
+#         nn.Linear(in_features, out_features, bias=True),
+#         nn.LayerNorm(out_features),
+#         nn.GELU(),
+#         nn.Dropout(p=p_drop),
+#     )
 
 
 class SkipBlock(nn.Module):
@@ -151,6 +151,7 @@ class LitUCEModel(L.LightningModule):
         mask = batch[1].to(self.device)
         X = batch[2].to(self.device)
         Y = batch[3]
+        batch_weights = batch[5]
 
         batch_sentences = self.pe_embedding(batch_sentences.long())
         X = self.pe_embedding(X.long())
@@ -160,7 +161,7 @@ class LitUCEModel(L.LightningModule):
         _, embedding = self.forward(batch_sentences, mask=mask)
 
         X = self.gene_embedding_layer(X)
-        return X, Y, embedding
+        return X, batch_weights, embedding
 
     def get_gene_embedding(self, genes):
         if self.protein_embeds is None:
@@ -242,11 +243,11 @@ class LitUCEModel(L.LightningModule):
             criterion = KLDivergenceLoss()
         else:
             raise ValueError(f"Loss {self.cfg.loss.name} not supported")
-        X, Y, embs = self._compute_embedding_for_batch(batch)
+        X, batch_weights, embs = self._compute_embedding_for_batch(batch)
         embs = embs.unsqueeze(1).repeat(1, X.shape[1], 1)
         combine = torch.cat((X, embs), dim=2)
         decs = self.binary_decoder(combine)
-        loss = criterion(input=decs.squeeze(), target=Y)
+        loss = criterion(input=decs.squeeze(), target=batch_weights)
         sch = self.lr_schedulers()
 
         for scheduler in sch._schedulers: