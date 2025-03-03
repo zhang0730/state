1e3cb740ef532707af829b6255b9bf9bff9fda0c
Rajesh Ilango <rajesh.ilango@arcinstitute.org>
2025-03-03 11:35:57 -0800

    Preprocessing script for scBasecamp

--- scripts/preprocess_scbasecamp.py
+++ scripts/preprocess_scbasecamp.py
@@ -1,10 +1,14 @@
 import os
 import fire
 import logging
-import h5py as h5
+import shutil
 
+import pandas as pd
 from pathlib import Path
 import urllib.request
+
+import torch
+from transformers import AutoTokenizer, AutoModel
 
 from vci.data.preprocess import Preprocessor
 from vci.data.gene_emb import create_genename_sequence_map
@@ -19,73 +23,77 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
+ref_genome_file_type = 'cds' # 'cdna'
+
 scBasecamp_dir_species = {
     "arabidopsis_thaliana":     {"name": "Arabidopsis_thaliana",
                                  "bio_class": "plantae",
-                                 "ref_genome": "https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/arabidopsis_thaliana/cdna/Arabidopsis_thaliana.TAIR10.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/arabidopsis_thaliana/{ref_genome_file_type}/Arabidopsis_thaliana.TAIR10.{ref_genome_file_type}.all.fa.gz"},
     "bos_taurus":               {"name": "Bos_taurus",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/bos_taurus/cdna/Bos_taurus.ARS-UCD1.3.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/bos_taurus/{ref_genome_file_type}/Bos_taurus.ARS-UCD1.3.{ref_genome_file_type}.all.fa.gz"},
     "caenorhabditis_elegans":   {"name": "Caenorhabditis_elegans",
                                  "bio_class": "invertebrate",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/caenorhabditis_elegans/{ref_genome_file_type}/Caenorhabditis_elegans.WBcel235.{ref_genome_file_type}.all.fa.gz"},
     "callithrix_jacchus":       {"name": "Callithrix_jacchus",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/callithrix_jacchus/cdna/Callithrix_jacchus.mCalJac1.pat.X.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/callithrix_jacchus/{ref_genome_file_type}/Callithrix_jacchus.mCalJac1.pat.X.{ref_genome_file_type}.all.fa.gz"},
     "danio_rerio":              {"name": "Danio_rerio",
                                  "bio_class": "fish",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/danio_rerio/cdna/Danio_rerio.GRCz11.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/danio_rerio/{ref_genome_file_type}/Danio_rerio.GRCz11.{ref_genome_file_type}.all.fa.gz"},
     "drosophila_melanogaster":  {"name": "Drosophila_melanogaster",
                                  "bio_class": "insecta",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/drosophila_melanogaster/cdna/Drosophila_melanogaster.BDGP6.46.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/drosophila_melanogaster/{ref_genome_file_type}/Drosophila_melanogaster.BDGP6.46.{ref_genome_file_type}.all.fa.gz"},
     "equus_caballus":           {"name": "Equus_caballus",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/equus_caballus/cdna/Equus_caballus.EquCab3.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/equus_caballus/{ref_genome_file_type}/Equus_caballus.EquCab3.0.{ref_genome_file_type}.all.fa.gz"},
     "gallus_gallus":            {"name": "Gallus_gallus",
                                  "bio_class": "aves",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/gallus_gallus/cdna/Gallus_gallus.bGalGal1.mat.broiler.GRCg7b.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/gallus_gallus/{ref_genome_file_type}/Gallus_gallus.bGalGal1.mat.broiler.GRCg7b.{ref_genome_file_type}.all.fa.gz"},
     "gorilla_gorilla":          {"name": "Gorilla_gorilla",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/gorilla_gorilla/cdna/Gorilla_gorilla.gorGor4.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/gorilla_gorilla/{ref_genome_file_type}/Gorilla_gorilla.gorGor4.{ref_genome_file_type}.all.fa.gz"},
     "heterocephalus_glaber":    {"name": "Heterocephalus_glaber",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/heterocephalus_glaber_female/cdna/Heterocephalus_glaber_female.Naked_mole-rat_maternal.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/heterocephalus_glaber_female/{ref_genome_file_type}/Heterocephalus_glaber_female.Naked_mole-rat_maternal.{ref_genome_file_type}.all.fa.gz"},
     "homo_sapiens":             {"name": "Homo_sapiens",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/{ref_genome_file_type}/Homo_sapiens.GRCh38.{ref_genome_file_type}.all.fa.gz"},
     "macaca_mulatta":           {"name": "Macaca_mulatta",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/macaca_mulatta/cdna/Macaca_mulatta.Mmul_10.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/macaca_mulatta/{ref_genome_file_type}/Macaca_mulatta.Mmul_10.{ref_genome_file_type}.all.fa.gz"},
     "mus_musculus":             {"name": "Mus_musculus",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/mus_musculus/cdna/Mus_musculus.GRCm39.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/mus_musculus/{ref_genome_file_type}/Mus_musculus.GRCm39.{ref_genome_file_type}.all.fa.gz"},
     "oryctolagus_cuniculus":    {"name": "Oryctolagus_cuniculus",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/oryctolagus_cuniculus/cdna/Oryctolagus_cuniculus.OryCun2.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/oryctolagus_cuniculus/{ref_genome_file_type}/Oryctolagus_cuniculus.OryCun2.0.{ref_genome_file_type}.all.fa.gz"},
     "oryza_sativa":             {"name": "Oryza_sativa",
                                  "bio_class": "plantae",
-                                 "ref_genome": "https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/oryza_sativa/cdna/Oryza_sativa.IRGSP-1.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/oryza_sativa/{ref_genome_file_type}/Oryza_sativa.IRGSP-1.0.{ref_genome_file_type}.all.fa.gz"},
     "ovis_aries":               {"name": "Ovis_aries",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/ovis_aries/cdna/Ovis_aries_rambouillet.ARS-UI_Ramb_v2.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/ovis_aries/{ref_genome_file_type}/Ovis_aries_rambouillet.ARS-UI_Ramb_v2.0.{ref_genome_file_type}.all.fa.gz"},
     "pan_troglodytes":          {"name": "Pan_troglodytes",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/pan_troglodytes/cdna/Pan_troglodytes.Pan_tro_3.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/pan_troglodytes/{ref_genome_file_type}/Pan_troglodytes.Pan_tro_3.0.{ref_genome_file_type}.all.fa.gz"},
     "schistosoma_mansoni":      {"name": "Schistosoma_mansoni",
                                  "bio_class": "trematoda",
-                                 "ref_genome": "http://ftp.ensemblgenomes.org/pub/metazoa/release-60/fasta/schistosoma_mansoni/cdna/Schistosoma_mansoni.Smansoni_v7.cdna.all.fa.gz"},
+                                 "ref_genome": f"http://ftp.ensemblgenomes.org/pub/metazoa/release-60/fasta/schistosoma_mansoni/{ref_genome_file_type}/Schistosoma_mansoni.Smansoni_v7.{ref_genome_file_type}.all.fa.gz"},
     "solanum_lycopersicum":     {"name": "Solanum_lycopersicum",
                                  "bio_class": "plantae",
-                                 "ref_genome": "https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/solanum_lycopersicum/cdna/Solanum_lycopersicum.SL3.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/solanum_lycopersicum/{ref_genome_file_type}/Solanum_lycopersicum.SL3.0.{ref_genome_file_type}.all.fa.gz"},
     "sus_scrofa":               {"name": "Sus_scrofa",
                                  "bio_class": "mammal",
-                                 "ref_genome": "https://ftp.ensembl.org/pub/release-113/fasta/sus_scrofa/cdna/Sus_scrofa.Sscrofa11.1.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensembl.org/pub/release-113/fasta/sus_scrofa/{ref_genome_file_type}/Sus_scrofa.Sscrofa11.1.{ref_genome_file_type}.all.fa.gz"},
     "zea_mays":                 {"name": "Zea_mays",
                                  "bio_class": "plantae",
-                                 "ref_genome": "https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/zea_mays/cdna/Zea_mays.Zm-B73-REFERENCE-NAM-5.0.cdna.all.fa.gz"},
+                                 "ref_genome": f"https://ftp.ensemblgenomes.ebi.ac.uk/pub/plants/release-60/fasta/zea_mays/{ref_genome_file_type}/Zea_mays.Zm-B73-REFERENCE-NAM-5.0.{ref_genome_file_type}.all.fa.gz"},
 }
 
 exclude_kingdom = ["plantae"]
+exclude_species = []
+# exclude_species = ["Homo_sapiens", 'Bos_taurus']
 
 # summary_file = '/scratch/ctc/ML/vci/scBasecamp_all.csv'
 summary_file = '/home/rajesh.ilango/scBasecamp_all.csv'
@@ -96,6 +104,9 @@ emb_idx_file = '/scratch/ctc/ML/uce/model_files/gene_embidx_mapping.torch'
 
 
 def download_ref_genome():
+    if not os.path.exists(geneome_loc):
+        os.makedirs(geneome_loc, exist_ok=True)
+
     for dataset, metadata in scBasecamp_dir_species.items():
         url = metadata['ref_genome']
         download_file = os.path.join(geneome_loc, Path(url).name)
@@ -123,6 +134,10 @@ def preprocess_scbasecamp(data_path='/large_storage/ctc/public/scBasecamp/GeneFu
     for species in species_dirs:
         mdata = scBasecamp_dir_species.get(species.lower())
         if mdata["bio_class"] in exclude_kingdom:
+            logging.warning(f"Skipping {species} as it belongs to {mdata['bio_class']}")
+            continue
+        if species in exclude_species:
+            logging.warning(f"Skipping {species} as it is in exclude list")
             continue
         log.info(f'Species {species}...')
         species_dir = Path(data_path) / species
@@ -136,6 +151,82 @@ def preprocess_scbasecamp(data_path='/large_storage/ctc/public/scBasecamp/GeneFu
                                   embedding_file,
                                   emb_idx_file)
         preprocess.process()
+
+
+def inferESM2(gene_emb_mapping_file='/large_storage/ctc/ML/data/cell/emb/ESM/gene_chrom_sequence_mapping.tsv',
+          output_file="/large_storage/ctc/ML/data/cell/emb/ESM/scBasecamp.gene_symbol_to_embedding_ESM2.pt",
+          batch_size=32):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Load the ESM2 model from Hugging Face
+    model_name = "facebook/esm2_t33_650M_UR50D"  # You can also use other ESM2 variants like "facebook/esm2_t12_35M_UR50D"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    model = model.to(device)
+    model.eval()
+
+    seq_df = pd.read_csv(gene_emb_mapping_file, sep='\t', header=None)
+    seq_df.columns = ['gene', 'chrom', 'sequence']
+
+    gene_emb_map = {}
+    if os.path.exists(output_file):
+        gene_emb_map= torch.load(output_file)
+
+    for i in range(0, len(seq_df), batch_size):
+        batch = seq_df.iloc[i:i+batch_size]
+
+        sequences = []
+        genes = []
+        for val1, val2 in zip(batch['gene'].tolist(),
+                              batch['sequence'].tolist()):
+            if val1 not in gene_emb_map:
+                # if len(val2) > 22168:
+                #     continue
+                logging.info(f"Processing {val1} = {len(val2)}")
+                genes.append(val1)
+                sequences.append(val2)
+            else:
+                logging.info(f"Skipping {val1}")
+
+        if len(sequences) == 0:
+            continue
+
+        # Tokenize the sequence
+        inputs = tokenizer(sequences, return_tensors="pt", padding=True).to(device)
+
+        # Generate embeddings
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        # Get the embeddings (representations)
+        # There are different ways to get embeddings from ESM2:
+        # Using the last hidden state (token embeddings)
+        token_embeddings = outputs.last_hidden_state
+
+        # Using the mean pooling of the last hidden state (sequence embedding)
+        # Mask the padding tokens
+        attention_mask = inputs["attention_mask"]
+        sequence_embeddings = torch.sum(token_embeddings * attention_mask.unsqueeze(-1), dim=1) / torch.sum(attention_mask, dim=1, keepdim=True)
+        sequence_embeddings = sequence_embeddings.cpu().numpy()
+        for gene, emb in zip(genes, sequence_embeddings):
+            gene_emb_map[gene] = emb
+
+        if i % (10 * batch_size) == 0:
+            logging.info(f'Saving after {i//batch_size} batches...')
+            torch.save(gene_emb_map, output_file)
+
+        if i % (1000 * batch_size) == 0:
+            logging.info(f'creating checkpoint {i//batch_size}...')
+            checkpoint_file = output_file.replace('.pt', f'.{i}.pt')
+            shutil.copyfile(output_file, checkpoint_file)
+
+        del token_embeddings
+        del outputs
+
+        # For per-residue embeddings (excluding special tokens)
+        # The first token is <cls> and the last is <eos>
+        # per_residue_embeddings = token_embeddings[0, 1:-1, :]
+        torch.cuda.empty_cache()
+    torch.save(gene_emb_map, output_file)
 
 if __name__ == '__main__':
     fire.Fire()
--- vci/data/gene_emb.py
+++ vci/data/gene_emb.py
@@ -5,28 +5,33 @@ from Bio import SeqIO
 
 def parse_genename_seq(fasta_file):
     gene_dict = {}
+    without_chromosome = 0
     with gzip.open(fasta_file, 'rt') as handle:
         for record in SeqIO.parse(handle, 'fasta'):
             header_parts = record.description.split()
             gene_name = None
             chromosome = None
-            i = 0
             for part in header_parts:
                 if part.startswith('gene_symbol:'):
                     gene_name = part.split(':')[1]
-                    i += 1
-                elif part.startswith('chromosome:') or part.startswith('chr:'):
-                    chromosome = part.split(':')[1]
-                    i += 1
-                if i == 2:
+                elif part.startswith('chromosome:'):
+                    chromosome = part.replace('chromosome:', '')
+                elif part.startswith('chr:'):
+                    chromosome = part.replace('chr:', '')
+                if gene_name and chromosome:
                     break
-            gene_dict[gene_name] = chromosome, str(record.seq)
-    return gene_dict
+
+            if gene_name is not None:
+                if chromosome is None:
+                    without_chromosome += 1
+                gene_dict[gene_name] = chromosome, str(record.translate().seq)
+
+    return gene_dict, without_chromosome
 
 
 def create_genename_sequence_map(fasta_file, output_file=None):
-    gene_dict = parse_genename_seq(fasta_file)
-    logging.info(f'{len(gene_dict)} genes found in {fasta_file}')
+    gene_dict, without_chromosome = parse_genename_seq(fasta_file)
+    logging.info(f'{len(gene_dict)} genes found in {fasta_file}. {without_chromosome} genes without chromosome information.')
     if output_file is not None:
         with open(output_file, 'a') as f:
             for gene, info in gene_dict.items():
