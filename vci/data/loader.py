fixed bug in normalization dim

vci/data/loader.py
@@ -371,9 +371,9 @@ class VCIDatasetSentenceCollator(object):
             counts = torch.log1p(counts)
 
         if counts.sum() == 0:
-            expression_weights = F.softmax(counts, dim=0)
+            expression_weights = F.softmax(counts, dim=1)
         else:
-            expression_weights = counts / torch.sum(counts, dim=0, keepdim=True)
+            expression_weights = counts / torch.sum(counts, dim=1, keepdim=True)
 
         ds_emb_idxs = self.dataset_to_protein_embeddings[dataset]
         if isinstance(ds_emb_idxs, np.ndarray):

vci/nn/model.py
@@ -149,9 +149,9 @@ class LitUCEModel(L.LightningModule):
         if self.cfg.model.counts:
             self.bin_encoder = nn.Embedding(10, d_model)
             self.count_encoder = nn.Sequential(
-                nn.Linear(1, 10, bias=True),
+                nn.Linear(1, 512, bias=True),
                 nn.LeakyReLU(),
-                nn.Linear(10, 10),
+                nn.Linear(512, 10),
             )
 
         if compiled: