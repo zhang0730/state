b3b368cd7d70bd166e82373c42cf87216305d1f3
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-05-28 13:22:13 -0700

    chore: added to __all__

--- src/state_sets/state/data/__init__.py
+++ src/state_sets/state/data/__init__.py
@@ -1,7 +1,9 @@
 from .loader import (
-    H5adSentenceDataset,
-    VCIDatasetSentenceCollator,
     GeneFilterDataset,
+    H5adSentenceDataset,
     NpzMultiDataset,
+    VCIDatasetSentenceCollator,
     create_dataloader,
 )
+
+__all__ = [H5adSentenceDataset, VCIDatasetSentenceCollator, GeneFilterDataset, NpzMultiDataset, create_dataloader]
