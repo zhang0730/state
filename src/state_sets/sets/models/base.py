1f48e0e98de46751124d0cf039988b9fc8ed9771
Abhinav Adduri <abhinadduri@gmail.com>
2025-06-05 18:58:44 -0700

    changed logic for decoder size to not overfit on smaller datasets

--- src/state_sets/sets/models/base.py
+++ src/state_sets/sets/models/base.py
@@ -145,10 +145,10 @@ class PerturbationModel(ABC, LightningModule):
             if gene_dim > 10000:
                 hidden_dims = [1024, 512, 256]
             else:
-                if self.control_pert == "non-targeting":
-                    hidden_dims = [1024, 1024, 512]  # make this config
-                else:
+                if 'DMSO_TF' in self.control_pert:
                     hidden_dims = [2048, 1024, 1024]
+                else:
+                    hidden_dims = [1024, 1024, 512]  # make this config
 
             self.gene_decoder = LatentToGeneDecoder(
                 latent_dim=self.output_dim,
